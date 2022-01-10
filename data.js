f061b5113478173cd6bd085639611c381d1c0b93
SimonLSC-SHF <96471303+SimonLSC-SHF@users.noreply.github.com>
2022-01-10 09:48:55 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,344 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-arouse---lbhc_1",
+      "name": "Arouse - LBHC_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012692758592919517,
+          "pitch": 0.17003081948366727,
+          "rotation": 0,
+          "target": "1-arouse---lbhc_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-arouse---lbhc_2",
+      "name": "Arouse - LBHC_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03224435260038483,
+          "pitch": 0.18104674009947175,
+          "rotation": 0,
+          "target": "2-arouse---lbhc_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-arouse---lbhc_3",
+      "name": "Arouse - LBHC_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0010447879982304897,
+          "pitch": 0.16442736646766853,
+          "rotation": 0,
+          "target": "3-arouse---lbhc_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-arouse---lbhc_4",
+      "name": "Arouse - LBHC_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.006903632859426523,
+          "pitch": 0.14360575869385883,
+          "rotation": 0,
+          "target": "4-arouse---lbhc_5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-arouse---lbhc_5",
+      "name": "Arouse - LBHC_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9370672464703382,
+          "pitch": 0.1759565639526084,
+          "rotation": 0,
+          "target": "5-arouse---lbhc_6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-arouse---lbhc_6",
+      "name": "Arouse - LBHC_6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.015542800511916255,
+          "pitch": 0.11275552318849691,
+          "rotation": 0,
+          "target": "6-arouse---lbhc_7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-arouse---lbhc_7",
+      "name": "Arouse - LBHC_7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.008882084111267119,
+          "pitch": 0.1346306003070552,
+          "rotation": 0,
+          "target": "7-arouse---lbhc_8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-arouse---lbhc_8",
+      "name": "Arouse - LBHC_8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012200812941010497,
+          "pitch": 0.15499572999687317,
+          "rotation": 0,
+          "target": "8-arouse---lbhc_9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-arouse---lbhc_9",
+      "name": "Arouse - LBHC_9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03551431360075341,
+          "pitch": 0.1039255897890996,
+          "rotation": 0,
+          "target": "9-arouse---lbhc_10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-arouse---lbhc_10",
+      "name": "Arouse - LBHC_10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Arouse-LBHC",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
